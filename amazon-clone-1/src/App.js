8c27916279294589e91957a60cf179710a76cf93
ttddww <woldeteketel@gmail.com>
2024-02-04 00:47:42 -0500

    carousel

--- amazon-clone-1/src/App.js
+++ amazon-clone-1/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Carousel from "./Components/Carousel/Carousel";
 function App() {
   return (
     <div>
     <Header/>
+    <Carousel/>
     </div>
   );
 }
