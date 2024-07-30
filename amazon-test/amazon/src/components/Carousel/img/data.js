b04d1e698d4bd1f294828f5cd147b874acdc7293
ttddww <152896246+ttddww@users.noreply.github.com>
2024-07-30 19:51:22 -0400

    Delete amazon-test directory

--- amazon-test/amazon/src/components/Carousel/img/data.js
+++
@@ -1,7 +0,0 @@
-import img1 from './400001136.jpg'
-import img2 from './400001169.jpg'
-import img3 from './400001202.jpg'
-import img4 from './400001235.jpg'
-import img5 from './400001268.jpg'
-
-export const img =[img1, img2, img3, img4, img5]
